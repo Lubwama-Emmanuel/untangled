c53805bb06b8784083bb2241dd3f9566d7f36166
Rex <lubwamaemmanuel1@gmail.com>
2023-09-11 22:34:52 +0300

    refactor:(refactored to Browser router)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import AppLayout from "./ui/AppLayout";
 import AboutMitch from "./pages/AboutMitch";
 
 export default function App() {
-  const router = createBrowserRouter([
-    {
-      element: <AppLayout />,
-      children: [
-        {
-          path: "/",
-          element: <Homepage />,
-        },
-        {
-          path: "/about_mitch",
-          element: <AboutMitch />,
-        },
-      ],
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     element: <AppLayout />,
+  //     children: [
+  //       {
+  //         path: "/",
+  //         element: <Homepage />,
+  //       },
+  //       {
+  //         path: "/about",
+  //         element: <AboutMitch />,
+  //       },
+  //     ],
+  //   },
+  // ]);
 
-  return <RouterProvider router={router} />;
+  // return <RouterProvider router={router} />;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/mitch" element={<AboutMitch />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
